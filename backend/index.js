ece4023db2131485efdd2ce93aca50086ff7a505
nicolofabris <nicolo.fabriss@gmail.com>
2023-09-15 11:49:42 +0200

    get and update books

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,46 @@ app.get('/books', async (req, res) => {
     }
 })
 
+app.get('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const book = await Book.findById(id)
+
+        return res.status(200).json(book)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: error.message})
+    }
+})
+
+app.put('/books/:id', async (req,res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+          ) {
+            return res
+              .status(400)
+              .send({ message: "Invalid fields: send all fields" });
+          }
+
+          const {id} = req.params
+
+          const result = await Book.findByIdAndUpdate(id, req.body)
+
+          if(!result) {
+            return res.status(404).json({message: 'Book not found'})
+          }
+          return res.status(200).send({message: 'Book updated successfully'})
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+    }
+})
+
 
 mongoose
   .connect(mongoDBURL)
